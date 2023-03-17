cb5285ee93667023d54d5d1e6e8edbd0df4ffba9
Nicholas <crazedglitch@yahoo.com>
2023-03-17 16:41:09 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Write your code in this file!
-
 const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
@@ -7,4 +6,3 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
-
